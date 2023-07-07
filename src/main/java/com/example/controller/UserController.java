17d6737e96459476bb6607d81bbc971e32572699
Ayush Khodankar <ayushkhodankar01@gmail.com>
2023-07-07 11:30:19 +0530

    first commit

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@PostMapping("/saveDetails")
+	public ResponseEntity<String> saveDetails(@RequestBody User user){
+		try {
+            User savedUser = userRepository.save(user);
+            return ResponseEntity.ok("User saved successfully with ID: " + savedUser.getId());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save user");
+        }
+		
+	}
+	
+
+}
---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,84 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	private String address;
+	private String mobile;
+	private String email;
+	private String pincode;
+	private String gender;
+
+	public int getId() {
+		return id;
+	}
+	public User() {}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", address=" + address + ", mobile=" + mobile + ", email=" + email
+				+ ", pincode=" + pincode + ", gender=" + gender + "]";
+	}
+	public User(int id, String name, String address, String mobile, String email, String pincode, String gender) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.mobile = mobile;
+		this.email = email;
+		this.pincode = pincode;
+		this.gender = gender;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+
+}
